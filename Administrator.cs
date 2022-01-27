ea85d57934ae578d8a20c8952173b861ab76ca54
NikitaRimitsen <77281903+NikitaRimitsen@users.noreply.github.com>
2022-01-27 10:51:04 +0200

    Add files via upload

---
+++ Administrator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kinoteatr_bilet
+{
+    public partial class Administrator : Form
+    {
+
+        static string conn = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\opilane\source\repos\Kino1\AppData\Kino_DB.mdf;Integrated Security=True";
+        /*Надо менять            ↑ ↑ ↑ ↑ ↑ ↑ ↑  вот это, если ты пересел за другой комп!!!!!!!!!*/
+        SqlConnection connect_to_DB = new SqlConnection(conn);
+
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        public Administrator()
+        {
+            this.ClientSize = new System.Drawing.Size(720, 500);
+
+
+            Button naita = new Button
+            {
+                Text = "Naita",
+                Location = new System.Drawing.Point(500, 100),//Point(x,y)
+                Height = 50,
+                Width = 120,
+                BackColor = Color.LightYellow
+            };
+            naita.Click += Film_naita_Click;
+
+            this.Controls.Add(naita);
+            /*TextBox nimi = new TextBox
+            {
+                Location = new System.Drawing.Point(50, 40),//Point(x,y)
+                Height = 80,
+                Width = 150,
+            };
+
+            TextBox film = new TextBox
+            {
+                Location = new System.Drawing.Point(50, 80),//Point(x,y)
+                Height = 80,
+                Width = 150,
+            };
+            this.Controls.Add(nimi);
+            this.Controls.Add(film);*/
+        }
+        private void Film_naita_Click(object sender, EventArgs e)
+        {
+            connect_to_DB.Open();
+            DataTable tabel = new DataTable();
+            DataGridView dataGridView = new DataGridView();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT Nimi,Aasta FROM [dbo].[Film]", connect_to_DB);
+            adapter.Fill(tabel);
+            dataGridView.DataSource = tabel;
+            dataGridView.Location = new System.Drawing.Point(10, 75);
+            dataGridView.Size = new System.Drawing.Size(400, 200);
+            this.Controls.Add(dataGridView);
+            connect_to_DB.Close();
+        }
+    }
+}
